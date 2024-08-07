49a249b3dddce1b4f021b5d0b83d910bb25f0121
caffreydev <joe.mccaffrey@hotmail.co.uk>
2024-08-07 17:32:37 +0100

    bfs on a graph weighted adjacency matrix

--- kata-machine/src/day1/BFSGraphMatrix.ts
+++ kata-machine/src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,43 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const length = graph.length;
+    const seen = new Array(length).fill(false)
+    const prev = new Array(length).fill(-1)
+
+    seen[source] = true
+    const q: number[] = [source]
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        const adjs = graph[curr]
+        for (let i = 0; i < adjs.length; i++) {
+            if (adjs[i] === 0) {
+                continue;
+            }
+            if (seen[i]) {
+                continue;
+            }
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i)
+        }
+        seen[curr] = true;
+
+
+    } while (q.length)
+
+    let curr = needle;
+    const out: number[] = []
+    while (prev[curr] !== -1) {
+        out.push(curr)
+        curr = prev[curr]
+    }
+    if (out.length) {
+        return [source].concat(out.reverse())
+    } else {
+        return null
+    }
+
 
 }
