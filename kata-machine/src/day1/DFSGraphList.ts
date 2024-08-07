aefcd1554f7d84d2d0559529d0bbeb0f8b81c432
caffreydev <joe.mccaffrey@hotmail.co.uk>
2024-08-07 18:57:33 +0100

    Depth first saearch on a graphlist

--- kata-machine/src/day1/DFSGraphList.ts
+++ kata-machine/src/day1/DFSGraphList.ts
@@ -1,3 +1,23 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+    if (seen[curr]) return false;
 
+    path.push(curr);
+    seen[curr] = true;
+    if (curr === needle) return true;
+    const list = graph[curr]
+    for (let i = 0; i < list.length; i++) {
+        const edge = list[i]
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true
+        }
+    }
+    path.pop()
+    return false
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false)
+    const path: number[] = []
+    walk(graph, source, needle, seen, path)
+    return path.length ? path : null
 }
